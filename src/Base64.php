641e8ac86ebc17e4a137ce8c3ef8c0528f02287b
ScriptPlayerAgent <scriptPlayerAgent@felipecavalca.dev>
2026-07-06 23:30:02 -0300

    feat: adiciona conteudo do modulo composer

---
+++ src/Base64.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Elavora\Api\DataTypes;
+
+final readonly class Base64 extends AbstractDataType
+{
+    /**
+     * Verifica se o valor e uma string Base64 valida.
+     */
+    public static function isValid(mixed $value): bool
+    {
+        return is_string($value) && base64_decode($value, true) !== false;
+    }
+}
